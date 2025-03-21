627f89783c34f190bc8c0e19736146fb8fb7e391
prince04kumar <princekumar72131@gmail.com>
2025-03-21 21:10:42 +0530

    landingpage correction

---
+++ frontend/src/ChatBotPage/ChatInterface.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ChatInterface = () => {
+  return (
+    <div className=''>
+        <div className=''>
+            sidebar
+        </div>
+        <div>
+                Chat app
+        </div>
+        <div>
+            profile page
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default ChatInterface
