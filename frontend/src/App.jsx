7068a48b1b82f882a131deb0711b6a65d8237ece
vedhem999 <161755713+vedantDube@users.noreply.github.com>
2025-03-21 21:22:20 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import LandingPage from './LandingPage'
+import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import LandingPage from "./LandingPage";
 
 const App = () => {
   return (
-    <div>
-      <LandingPage/>
-      
-      
-    </div>
-  )
-}
+    <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <div>
+        <LandingPage />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
