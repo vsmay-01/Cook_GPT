22ef1eb9e4319d91bf012ac712b59be2e64ce43a
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2026-01-21 01:39:44 +0530

    Update ProfileSec.jsx

--- frontend/src/ChatBotPage/BotPageCom/ProfileSec.jsx
+++ frontend/src/ChatBotPage/BotPageCom/ProfileSec.jsx
@@ -35,26 +35,26 @@ const ProfileSec = () => {
       </SignedIn>
     </div>
 
-     {/* Reranked Documents Section */}
-      {/* <SignedIn> */}
-        {/* <div className="mt-10 text-sm text-gray-300"> */}
-          {/* <h3 className="font-semibold text-[#3B82F6] text-xs md:text-sm"> */}
-            {/* Reranked Documents: */}
-          {/* </h3> */}
-          {/* <div className="mt-2 max-h-[60vh] overflow-y-auto scrollbar-thin scrollbar-thumb-[#3B82F6] scrollbar-track-[#1A1A1A]"> */}
-      {/* {rerankedDocuments?.length > 0 ? (
-              <p className="text-gray-200 text-xs md:text-sm whitespace-pre-wrap">
-                {rerankedDocuments}
-              </p>
-            ) : (
-              <p className="text-gray-400 text-xs md:text-sm">
-                No documents available.
-              </p>
-            )} */}
-          {/* </div> */}
-        {/* </div> */}
-      {/* </SignedIn>  */}
-    /*</div>*/
+     // {/* Reranked Documents Section */}
+      // {/* <SignedIn> */}
+        // {/* <div className="mt-10 text-sm text-gray-300"> */}
+          // {/* <h3 className="font-semibold text-[#3B82F6] text-xs md:text-sm"> */}
+            // {/* Reranked Documents: */}
+          // {/* </h3> */}
+          // {/* <div className="mt-2 max-h-[60vh] overflow-y-auto scrollbar-thin scrollbar-thumb-[#3B82F6] scrollbar-track-[#1A1A1A]"> */}
+      // {/* {rerankedDocuments?.length > 0 ? (
+              // <p className="text-gray-200 text-xs md:text-sm whitespace-pre-wrap">
+                // {rerankedDocuments}
+              // </p>
+            // ) : (
+              // <p className="text-gray-400 text-xs md:text-sm">
+                // No documents available.
+              // </p>
+            // )} */}
+          // {/* </div> */}
+        // {/* </div> */}
+      // {/* </SignedIn>  */}
+    // /*</div>*/
   );
 };
 
